a86ab54e2b27a02b386ff2cc22fb92f58bf79f00
CarstenHennig <carsten.hennig@gmail.com>
2021-06-23 16:22:57 +0200

    Just under construction

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,25 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import "../styles/main.scss";
+// import "../styles/main.scss";
 
 // Import any additional modules you want to include below \/
-import moment from "moment";
+import moment from "../../node_modules/moment";
 
 // \/ All of your javascript should go here \/
-const days = moment().format("Do");
+const days = moment().format("LL");
 const days_left = document.getElementById("days_left");
-days.innerHTML = days_left;
+days_left.innerText = days;
 
-const hours = moment().format("h");
+const hours = moment().endOf("day").fromNow();
 const hours_left = document.getElementById("hours_left");
-hours.innerHTML = hours_left;
+hours_left.innerHTML = hours;
 
-const minutes = moment().format("mm");
+const minutes = moment().endOf("hour").fromNow();
 const minutes_left = document.getElementById("minutes_left");
-minutes.innerHTML = minutes_left;
+minutes_left.innerHTML = minutes;
 
-const seconds = moment().format("ss");
+const seconds = moment().endOf("minutes").fromNow();
 const seconds_left = document.getElementById("seconds_left");
-seconds.innerHTML = seconds_left;
+seconds_left.innerHTML = seconds;
 
 // script countdown function
 var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
